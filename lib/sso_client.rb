471379c02042be5d586cd77a72db3ce2d3b3056c
gutomcosta <gutomcosta@gmail.com>
2017-03-23 15:09:37 -0300

    adds helper to controll user already logged session on controller

--- lib/sso_client.rb
+++ lib/sso_client.rb
@@ -5,9 +5,11 @@ require "sso_client/invalid_token_error"
 require "sso_client/sso_server_not_reachable_error"
 require "sso_client/sso_configuration"
 require "sso_client/session_handler"
+require "sso_client/user_logged_session_handler"
 require "sso_client/controller_helpers"
 require "sso_client/crypto_adapter"
 require "sso_client/user_repository"
+
 
 
 module SsoClient
